ca6ff4d940149e6a4f4dd723cf93338634e27fb2
Hosea kipngetich <96489816+kipkoech88@users.noreply.github.com>
2024-05-31 09:44:56 +0000

    setup dimensions

---
+++ main.lua
@@ -0,0 +1,10 @@
+-- get push library
+push = require "push"
+
+-- set screen dimensions
+WINDOW_WIDTH = 1288
+WINDOW_HEGHT = 720
+
+--SET virtual dimensions
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
